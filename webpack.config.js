eb8ee8e90fc62814a5a4bcd5ae205a736fde8bf2
Marty McLaughlin <martymclaugh@gmail.com>
2016-10-22 15:26:10 -0700

    add comma

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname,
   entry: './js/ClientApp.js',
   output: {
-    path: path.join(__dirname, '/public')
+    path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
   },
   resolve: {
