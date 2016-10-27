ce0b17c3a33204c5d7055349929a7482679dc03b
Marty McLaughlin <martymclaugh@gmail.com>
2016-10-27 15:36:10 -0700

    finish search method

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -3,9 +3,11 @@ const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const Search = require('./Search')
 const Layout = require('./Layout')
-const { Router, Route, IndexRoute, hashHistory } = require('react-router')
 const Details = require('./Details')
+const { Router, Route, IndexRoute, hashHistory } = require('react-router')
 const { shows } = require('../public/data')
+const { store } = require('./Store')
+const { Provider } = require('react-redux')
 
 const App = React.createClass({
   assignShow (nextState, replace) {
@@ -20,14 +22,17 @@ const App = React.createClass({
   },
   render () {
     return (
-      <Router history={hashHistory}>
-        <Route path='/' component={Layout}>
-          <IndexRoute component={Landing} />
-          <Route path='/search' component={Search} shows={shows} />
-          <Route path='/details/:id' component={Details} onEnter={this.assignShow} />
-        </Route>
-      </Router>
+      <Provider store={store}>
+        <Router history={hashHistory}>
+          <Route path='/' component={Layout}>
+            <IndexRoute component={Landing} />
+            <Route path='/search' component={Search} shows={shows} />
+            <Route path='/details/:id' component={Details} onEnter={this.assignShow} />
+          </Route>
+        </Router>
+      </Provider>
     )
   }
 })
+
 ReactDOM.render(<App />, document.getElementById('app'))
